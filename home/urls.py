3cb94b710d1fa192b18cd4818789ccde8f3d3191
YashGarg14 <yash.raj.garg2003@gmail.com>
2022-11-09 21:49:00 +0530

    Login and signup added

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about', views.about, name = 'about'),
     path('contact', views.contact, name = 'contact'),
+    path('signup', views.handleSignUp, name="handleSignUp"),
+    path('login', views.handeLogin, name="handleLogin"),
+    path('logout', views.handelLogout, name="handleLogout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from home.models import Contact
 from django.contrib import messages
+from django.contrib.auth.models import User
+from Blogg.models import Post
+from django.contrib.auth  import authenticate,  login, logout
 
 # Create your views here.
 
@@ -21,5 +24,66 @@ def contact(request):
             contact.save()
             messages.success(request, "Your message has been successfully sent")
     return render(request, "home/contact.html")
+
 def about(request):
     return render(request, 'home/about.html')
+
+def handleSignUp(request):
+    if request.method=="POST":
+        # Get the post parameters
+        username=request.POST['username']
+        email=request.POST['email']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+
+        # check for errorneous input
+        if len(username)<10:
+            messages.error(request, " Your user name must be under 10 characters")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request, " User name should only contain letters and numbers")
+            return redirect('home')
+        if (pass1!= pass2):
+             messages.error(request, " Passwords do not match")
+             return redirect('home')
+
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name= fname
+        myuser.last_name= lname
+        myuser.save()
+        messages.success(request, " Your Blogg account has been successfully created")
+        return redirect('home')
+
+    else:
+        return HttpResponse("404 - Not found")
+    return HttpResponse("login")
+
+def handeLogin(request):
+    if request.method=="POST":
+        # Get the post parameters
+        loginusername=request.POST['loginusername']
+        loginpassword=request.POST['loginpassword']
+
+        user=authenticate(username= loginusername, password= loginpassword)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid credentials! Please try again")
+            return redirect("home")
+
+    return HttpResponse("404- Not found")
+
+def handelLogout(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('home')
+
+
+def about(request): 
+    return render(request, "home/about.html")
